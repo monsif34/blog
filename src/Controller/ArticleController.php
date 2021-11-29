74b427c490de823e5180630807882124afc050da
monsif34 <monsif.zourgui@epsi.fr>
2021-11-29 14:17:42 +0100

    ajout formulaire avec controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,21 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    private $articleRepository;
+
+    public function __construct(ArticleRepository $articleRepository)
+    {
+       $this->articleRepository = $articleRepository;
+    }
+
     /**
-     * @Route("/Nos-articles", name="article")
+     * @Route("/Nos-articles/", name="article")
      */
     public function index(): Response
     {
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'controller_name' => 'arcticle',
+            'articles' => $this->articleRepository->findAll(),
         ]);
     }
 
-    private $articleRepository;
+    /**
+     * @Route("/Nos-articles/{id}", name="articleNumber")
+     */
+    public function ArticleById(string $id): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'arcticle',
+            'articlesNumber' => $this->articleRepository->find($id),
+        ]);
+    }
+
+    /**
+     * @Route("/ajouterArticle", name="ajouterArticle")
+     */
+    public function new(Request $request): Response
+    {
+        // just setup a fresh $task object (remove the example data)
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+
+
+
+        return $this->renderForm('article/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
 }
